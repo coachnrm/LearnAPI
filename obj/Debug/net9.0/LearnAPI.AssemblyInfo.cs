c886aa6806b5f921b6b05b61ce90b23f08f4d515
coachnrm <nuttapong_coach@hotmail.com>
2024-12-25 12:59:33 +0700

    add code

--- obj/Debug/net9.0/LearnAPI.AssemblyInfo.cs
+++ obj/Debug/net9.0/LearnAPI.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("LearnAPI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+862b77e8157d2a4e90d79b79aeeb968f41d27ba8")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+44d22a25f51461339485d9488efbaf46c7e22326")]
 [assembly: System.Reflection.AssemblyProductAttribute("LearnAPI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("LearnAPI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
